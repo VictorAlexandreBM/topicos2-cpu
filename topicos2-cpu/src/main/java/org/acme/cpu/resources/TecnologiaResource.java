c4a26f92033020d8cddf2a820116be6d956fbdd9
Victor Alexandre B M <valex.milhomem@gmail.com>
2026-03-22 11:32:13 -0300

    feat: Cria resource de tecnologia

---
+++ topicos2-cpu/src/main/java/org/acme/cpu/resources/TecnologiaResource.java
@@ -0,0 +1,45 @@
+package org.acme.cpu.resources;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.validation.Valid;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.acme.cpu.dto.Tecnologia.TecnologiaDTO;
+import org.acme.cpu.services.TecnologiaService;
+
+
+@ApplicationScoped
+@Path("/tecnologias")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class TecnologiaResource {
+
+    @Inject
+    TecnologiaService service;
+
+    @GET
+    public Response listar() {
+        return Response.ok(service.listar()).build();
+    }
+
+    @POST
+    public Response inserir(@Valid TecnologiaDTO dto) {
+        return Response.ok(service.criar(dto)).build();
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response atualizar(@PathParam("id") Long id, @Valid TecnologiaDTO dto) {
+        service.atualizar(id, dto);
+        return Response.noContent().build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response deletar(@PathParam("id") Long id) {
+        service.deletar(id);
+        return Response.noContent().build();
+    }
+}
