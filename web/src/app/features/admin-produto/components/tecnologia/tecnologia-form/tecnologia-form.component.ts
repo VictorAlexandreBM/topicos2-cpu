90e9b86ab027b7f29ad5548662d06cf77a93d3a3
Victor Alexandre B M <valex.milhomem@gmail.com>
2026-03-22 16:11:24 -0300

    feat: cria componente de formulário

---
+++ web/src/app/features/admin-produto/components/tecnologia/tecnologia-form/tecnologia-form.component.ts
@@ -0,0 +1,42 @@
+import {Component, inject} from '@angular/core';
+import {FormBuilder, NonNullableFormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {MatFormField, MatInput, MatLabel} from '@angular/material/input';
+import {MatButton} from '@angular/material/button';
+import {MatGridList, MatGridTile} from '@angular/material/grid-list';
+import TecnologiaService from '../../../services/tecnologia.service';
+import {TecnologiaFormRequest} from '../../../models/tecnologia.model';
+
+@Component({
+  selector: 'app-tecnologia-form',
+  templateUrl: './tecnologia-form.component.html',
+  imports: [
+    ReactiveFormsModule,
+    MatFormField,
+    MatInput,
+    MatLabel,
+    MatButton,
+  ]
+})
+export class TecnologiaFormComponent {
+  private readonly fb =   inject(NonNullableFormBuilder);
+  private readonly service = inject(TecnologiaService);
+
+  protected readonly nomeCtrl = this.fb.control('', [Validators.required, Validators.maxLength(100)]);
+  protected readonly descricaoCtrl = this.fb.control('', [Validators.required, Validators.maxLength(255)]);
+
+  protected readonly tecnologiaForm = this.fb.group({
+    nome: this.nomeCtrl,
+    descricao: this.descricaoCtrl,
+  });
+
+  cadastrar() {
+    const dadosTratados = this.tratarDados(this.tecnologiaForm.value);
+
+    this.service.cadastrar(dadosTratados).subscribe((t) => console.log('Tecnologia cadastrada com sucesso!'))
+
+  }
+
+  tratarDados(dados: object) {
+    return dados as TecnologiaFormRequest;
+  }
+}
