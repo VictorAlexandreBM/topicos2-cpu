3923f6cb1bb6a09b31a6cc09a196f8f3d3ad251d
Victor Alexandre B M <valex.milhomem@gmail.com>
2026-03-21 23:33:29 -0300

    feat: adiciona proteções de banco de dados à classe tecnologia

--- topicos2-cpu/src/main/java/org/acme/cpu/models/Tecnologia.java
+++ topicos2-cpu/src/main/java/org/acme/cpu/models/Tecnologia.java
@@ -2,14 +2,19 @@ package org.acme.cpu.models;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 
 @Entity
-public class Tecnologia {
+public class Tecnologia extends BaseEntity {
 
-    @Column(unique = true, nullable = false)
+    @Column(unique = true, nullable = false, length = 100)
+    @NotBlank
+    @Size(max = 100)
     private String nome;
 
     @Column(nullable = true)
+    @Size(max = 255)
     private String descricao;
 
     public String getDescricao() {
