ac12c0707e98c8cd95624242cc0625032548ae91
Victor Alexandre B M <valex.milhomem@gmail.com>
2026-03-22 00:53:39 -0300

    feat: Cria entidade de socket

---
+++ topicos2-cpu/src/main/java/org/acme/cpu/models/Socket.java
@@ -0,0 +1,24 @@
+package org.acme.cpu.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Socket extends BaseEntity {
+
+    @Column(unique = true, nullable = false, length = 100)
+    @NotBlank
+    private String tipo;
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+
+}
