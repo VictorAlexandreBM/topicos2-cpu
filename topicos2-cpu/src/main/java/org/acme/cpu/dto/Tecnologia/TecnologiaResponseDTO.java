1a50044194504bbfa9139e0eb03450d730725e8c
Victor Alexandre B M <valex.milhomem@gmail.com>
2026-03-22 11:29:50 -0300

    feat: Cria dtos de requisição e resposta de tecnologia

---
+++ topicos2-cpu/src/main/java/org/acme/cpu/dto/Tecnologia/TecnologiaResponseDTO.java
@@ -0,0 +1,17 @@
+package org.acme.cpu.dto.Tecnologia;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import org.acme.cpu.models.Tecnologia;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+public record TecnologiaResponseDTO(
+
+        Long id,
+        String nome,
+        String descricao
+) {
+    public TecnologiaResponseDTO(Tecnologia t) {
+        this(t.getId(), t.getNome(), t.getDescricao());
+    }
+}
