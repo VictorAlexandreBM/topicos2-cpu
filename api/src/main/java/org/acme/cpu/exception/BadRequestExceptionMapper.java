503266ca1759cc7dd315de295337df839fcac966
Victor Alexandre B M <valex.milhomem@gmail.com>
2026-04-02 22:31:13 -0300

    feat: adiciona outras exceptions

---
+++ api/src/main/java/org/acme/cpu/exception/BadRequestExceptionMapper.java
@@ -0,0 +1,46 @@
+package org.acme.cpu.exception;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.BadRequestException;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
+
+import java.time.OffsetDateTime;
+
+@Provider
+@ApplicationScoped
+public class BadRequestExceptionMapper implements ExceptionMapper<BadRequestException> {
+
+    @ConfigProperty(name = "problem.base-url")
+    String baseUrl;
+
+    @Context
+    UriInfo uriInfo;
+
+    @Inject
+    Logger LOG;
+
+    @Override
+    public Response toResponse(BadRequestException e) {
+        var p = new Problem();
+        p.type = baseUrl + "/errors/bad-request";
+        p.title = "Requisição negada";
+        p.status = Response.Status.BAD_REQUEST.getStatusCode();
+        p.detail = e.getMessage();
+        p.instance = (uriInfo != null ? uriInfo.getRequestUri().getPath() : null);
+        p.timestamp = OffsetDateTime.now();
+
+        LOG.warnf("Bad Request (400) na URL '%s': %s", p.instance, e.getMessage());
+
+        return Response.status(p.status)
+                .type("application/problem+json")
+                .entity(p)
+                .build();
+    }
+}
---
+++ api/src/main/java/org/acme/cpu/exception/UncaughtExceptionMapper.java
@@ -0,0 +1,47 @@
+package org.acme.cpu.exception;
+
+import io.quarkus.arc.profile.IfBuildProfile;
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.core.Context;
+import jakarta.ws.rs.core.Response;
+import jakarta.ws.rs.core.UriInfo;
+import jakarta.ws.rs.ext.ExceptionMapper;
+import jakarta.ws.rs.ext.Provider;
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.jboss.logging.Logger;
+
+import java.time.OffsetDateTime;
+
+@Provider
+@ApplicationScoped
+@IfBuildProfile("prod")
+public class UncaughtExceptionMapper implements ExceptionMapper<Throwable> {
+
+    @ConfigProperty(name = "problem.base-url")
+    String baseUrl;
+
+    @Context
+    UriInfo uriInfo;
+
+    @Inject
+    Logger LOG;
+
+    @Override
+    public Response toResponse(Throwable t) {
+        LOG.errorf(t, "Erro inesperado em %s", (uriInfo != null ? uriInfo.getRequestUri() : "<no-uri>"));
+
+        Problem p = new Problem();
+        p.type = baseUrl + "/errors/unexpected-error";
+        p.title = "Erro inesperado";
+        p.status = Response.Status.INTERNAL_SERVER_ERROR.getStatusCode();
+        p.detail = "Ocorreu um erro inesperado. Tente novamente mais tarde.";
+        p.instance = (uriInfo != null ? uriInfo.getRequestUri().getPath() : null);
+        p.timestamp = OffsetDateTime.now();
+
+        return Response.status(p.status)
+                .type("application/problem+json")
+                .entity(p)
+                .build();
+    }
+}
