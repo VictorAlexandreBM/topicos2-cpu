cab84814e7b2bb65f29d5eeb22d53fcc938acb99
Victor Alexandre B M <valex.milhomem@gmail.com>
2026-04-03 15:24:05 -0300

    feat: cria service genérico

--- web/src/app/features/admin-produto/services/tecnologia.service.ts
+++ web/src/app/features/admin-produto/services/tecnologia.service.ts
@@ -4,41 +4,11 @@ import {Tecnologia, TecnologiaFormRequest} from '../models/tecnologia.model';
 import {delay, Observable} from 'rxjs';
 import {RespostaPaginada} from '../../../core/models/resposta-paginada.model';
 import {ParametrosListagem} from '../../../core/models/parametros-lista.model';
+import BaseCrudService from '../../../core/services/base-crud.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export default class TecnologiaService {
-
-  private readonly http = inject(HttpClient);
-
-  listar(parametrosListagem: ParametrosListagem): Observable<RespostaPaginada<Tecnologia[]>> {
-
-    const parametrosFiltrados = Object.fromEntries(
-      Object.entries(parametrosListagem).filter(([_, v]) =>
-        (v !== undefined && v !== null && v !== '')
-      )
-    ) as { [key: string]: string | number | boolean}
-
-
-    return this.http.get<RespostaPaginada<Tecnologia[]>>('tecnologias', {
-      params: parametrosFiltrados
-    });
-  }
-
-  cadastrar(dados: TecnologiaFormRequest): Observable<Tecnologia> {
-    return this.http.post<Tecnologia>('tecnologias', dados);
-  }
-
-  deletar(id: number) {
-    return this.http.delete(`tecnologias/${id}`);
-  }
-
-  alterarEstado(id: number, ativo: boolean) {
-    return this.http.patch(`tecnologias/${id}`, {ativo});
-  }
-
-  atualizar(id: number, dados: TecnologiaFormRequest) {
-    return this.http.put<Tecnologia>(`tecnologias/${id}`, dados);
-  }
+export default class TecnologiaService extends BaseCrudService<Tecnologia, TecnologiaFormRequest> {
+  protected readonly recurso = 'tecnologias';
 }
