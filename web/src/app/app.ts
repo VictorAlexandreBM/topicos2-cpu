ab1fe2e1f9acbe5a8142cca5a5e0202799a38116
Victor Alexandre B M <valex.milhomem@gmail.com>
2026-04-03 11:43:56 -0300

    feat: adiciona layout melhorado

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -1,9 +1,13 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
+import {SideNavComponent} from './core/components/layout/side-nav/side-nav.component';
+import {MatSidenav, MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
+import {MatButton, MatIconButton} from '@angular/material/button';
+import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SideNavComponent, MatSidenavContainer, MatSidenav, MatButton, RouterLink, RouterLinkActive, MatSidenavContent, MatIconButton, MatIcon],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
